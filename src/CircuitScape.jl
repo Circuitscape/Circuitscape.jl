7ad0c577604627dcc5409fcbec207b831c392607
ranjanan <benditlikeranjan@gmail.com>
2017-02-27 15:39:50 +0530

    Work with PyAMG solver

--- src/CircuitScape.jl
+++ src/CircuitScape.jl
@@ -1,6 +1,11 @@
 module CircuitScape
 
 using IniFile
+using PyAMG
+using Logging
+using LightGraphs
+using IterativeSolvers
+Logging.configure(level = DEBUG)
 
 include("config.jl")
 include("io.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -10,9 +10,15 @@ using Base.Test
 
 #@test a ≈ r
 
+# Simple test with one connected component
+r = compute("sgNetworkVerify2.ini")
+x = readdlm("sgNetworkVerify2_resistances.out")
+x = x[2:end, 2:end]
+
+@test sumabs2(x - r) < 1e-6
+
 # Network test with multiple connected components
 r = compute("sgNetworkVerify1.ini")
-
 x = readdlm("sgNetworkVerify1_resistances.out")
 x = x[2:end, 2:end]
 
