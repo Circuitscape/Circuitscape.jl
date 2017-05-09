1bec5424eb74aff5c3d964b048d2a861f4bf5821
ranjanan <benditlikeranjan@gmail.com>
2017-05-09 14:45:16 +0530

    Compute branch and node currents

--- src/network.jl
+++ src/network.jl
@@ -1,4 +1,5 @@
-function single_ground_all_pair_resistances{T}(a::SparseMatrixCSC, g::Graph, c::Vector{T}; exclude = Tuple{Int,Int}[])
+function single_ground_all_pair_resistances{T}(a::SparseMatrixCSC, g::Graph, c::Vector{T}; 
+                                                    exclude = Tuple{Int,Int}[])
     numpoints = size(c, 1)
     cc = connected_components(g)
     debug("Graph has $(size(a,1)) nodes, $numpoints focal points and $(length(cc)) connected components")
@@ -49,7 +50,7 @@ function single_ground_all_pair_resistances{T}(a::SparseMatrixCSC, g::Graph, c::
                 cg!(v, cond_pruned, curr, M; tol = 1e-6, maxiter = 100000)
                 curr[:] = 0
             end
-            postprocess(v, c, i, j, resistances, pt1, pt2)
+            postprocess(v, c, i, j, resistances, pt1, pt2, cond_pruned, cc[rcc])
             v[:] = 0
         end
         cond_pruned[pt1,pt1] = d
@@ -78,7 +79,7 @@ function laplacian(G::SparseMatrixCSC)
     G = -G + spdiagm(vec(sum(G, 1)))
 end
 
-function postprocess(volt, cond, i, j, resistances, pt1, pt2)
+function postprocess(volt, cond, i, j, resistances, pt1, pt2, cond_pruned, cc)
     #fname = "/tmp/voltages_$(p1)_$(p2).txt"
 
     #=open(fname, "a") do f
@@ -89,6 +90,8 @@ function postprocess(volt, cond, i, j, resistances, pt1, pt2)
     end=#
 
     r = resistances[i, j] = resistances[j, i] = volt[pt2] - volt[pt1]
+
+    write_cur_maps(cond_pruned, volt, [-9999.], cond, cc)
 end
 
 function compute_network(a)
--- src/out.jl
+++ src/out.jl
@@ -12,3 +12,83 @@ function compute_3col{T}(resistances::Matrix{T}, fp)
     end
     r3col
 end
+
+function write_cur_maps(cond, voltages, finitegrounds, pts, cc)
+
+    node_currents, branch_currents = _create_current_maps(cond, voltages, finitegrounds)
+    branch_currents_array = _convert_to_3col(branch_currents, cc, pts)
+end
+
+function _convert_to_3col(branch_currents, cc, fp)
+
+    l = length(branch_currents.nzval)
+    graph = zeros(l, 3)
+    graph[:,3] = branch_currents.nzval
+    idx = find(branch_currents)
+    I,J,V = findnz(branch_currents)
+
+    for i = 1:size(I, 1)
+        graph[i,1] = cc[I[i]]
+        graph[i,2] = cc[J[i]]
+    end
+
+    graph
+end
+
+function _create_current_maps(G, voltages, finitegrounds)
+
+    node_currents = get_node_currents(G, voltages, finitegrounds)
+    branch_currents = _get_branch_currents(G, voltages, true)
+    branch_currents = abs(branch_currents)
+
+    node_currents, branch_currents
+end
+
+function get_node_currents(G, voltages, finitegrounds)
+
+    node_currents_pos = _get_node_currents_posneg(G, voltages, finitegrounds, true)
+    node_currents_neg = _get_node_currents_posneg(G, voltages, finitegrounds, false)
+    node_currents = map((x,y) -> x > y ? x : y, node_currents_pos, node_currents_neg)
+end
+
+function _get_node_currents_posneg(G, voltages, finitegrounds, pos)
+
+    branch_currents = _get_branch_currents(G, voltages, pos)
+    branch_currents = branch_currents - branch_currents'
+    I,J,V = findnz(branch_currents)
+    mask = V .> 0
+    n = size(G, 1)
+    branch_currents = sparse(I[mask], J[mask], V[mask], n, n)
+    sum(branch_currents, 1)
+end
+
+function _get_branch_currents(G, voltages, pos)
+
+	branch_currents = _get_branch_currents_posneg(G, voltages, pos)
+	I,J,V = findnz(G)
+    n = size(G, 1)
+	mask = I .< J
+	branch_currents = sparse(I[mask], J[mask], branch_currents, n, n)
+
+	branch_currents  
+end
+
+function _get_branch_currents_posneg{T}(G, v::Vector{T}, pos)
+    I,J,V = findnz(G)
+    mask = I .< J
+    vdiff = zeros(T, sum(mask))
+    if pos
+        vdiff = v[I[mask]] - v[J[mask]]
+        #for (i,v) in enumerate(find(mask))
+        #    vdiff[i] = v[I[v]] - v[J[v]]
+        #end
+    else
+        vdiff = v[J[mask]] - v[I[mask]]
+        #for (i,v) in enumerate(find(mask))
+        #    vdiff[i] = v[J[v]] - v[I[v]]
+        #end
+    end
+    map!(x -> x < 0 ? -x : 0, V)
+
+    branch_currents = vdiff .* V[mask]
+end
