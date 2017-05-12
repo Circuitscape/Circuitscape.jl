ec2f4e6d9415cbe9dbc0c1defe3e71700dcabf4e
ranjanan <benditlikeranjan@gmail.com>
2017-05-12 18:32:21 +0530

    Add test for sgVerify2 current maps

---
+++ test/compare_output.jl
@@ -0,0 +1,33 @@
+function compare_all_output(str)
+
+    gen_list, list_to_comp = generate_lists(str)
+
+    for f in gen_list
+        @show f
+        if endswith(f, "asc")
+            r = read_aagrid("output/$f")
+            println("done with me")
+            x = get_comp(list_to_comp, f)
+            println("done with you")
+            @show compare_aagrid(r, x)
+            @test compare_aagrid(r, x)
+        end
+    end
+            
+end
+
+list_of_files(str, pref) = readdir(pref) |> y -> filter(x -> startswith(x, str), y)
+generate_lists(str) = list_of_files(str, "output/"), list_of_files(str, "output_verify/")
+
+read_aagrid(file) = readdlm(file, skipstart = 0) # Will change to 6 
+
+compare_aagrid{T}(r::Matrix{T}, x::Matrix{T}) = sumabs2(x - r) < 1e-6
+
+function get_comp(list_to_comp, f)
+    outfile = ""
+    if f in list_to_comp
+        outfile = "output_verify/$f"
+    end
+    readdlm(outfile; skipstart = 6)
+end
+
