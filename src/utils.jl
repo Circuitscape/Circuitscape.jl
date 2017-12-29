e057c85d17f652b3ea396b0a27a3d455370707c4
ranjanan <benditlikeranjan@gmail.com>
2017-12-29 10:06:43 -0800

    model problem

--- src/utils.jl
+++ src/utils.jl
@@ -1,15 +1,18 @@
-function model_problem(size, el = Float64)
+export model_problem
+
+function model_problem(T, s)
     
     # Cell map is the uniform 1 across the grid
-    cellmap = ones(el, size, size)
+    cellmap = ones(T, s, s)
     
     # Nodemap is just 1:endof(cellmap)
-    nodemap = reshape(1:endof(cellmap), size(cellmap)...)
+    nodemap = reshape(1:endof(cellmap), s, s) |> collect
 
     # Construct graph with 4 neighbors
-    G = construct_graph(cellmap, nodemap, true, false)
+    G = construct_graph(cellmap, nodemap, true, true)
 
     # Return laplacian
     laplacian(G)
 end
+model_problem(s::Integer) = model_problem(Float64, s)
     
