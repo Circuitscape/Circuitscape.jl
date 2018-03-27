c92408ea3c5ae89bc17b2f5228614de471cfa1a6
ranjanan <benditlikeranjan@gmail.com>
2018-03-28 01:33:21 +0530

    Data type and modelling mode

--- src/ui/ui.jl
+++ src/ui/ui.jl
@@ -12,39 +12,47 @@ w = Window()
 function generate_ui(w)
 
     heading = Node(:div, tachyons_css, "Circuitscape 5.0") |> 
-                    class"f-subheadline tc red"
+                    class"f-subheadline lh-title tc red"
 
-    line = Node(:hr)
+    section1 = Node(:div, tachyons_css, "Data Type and Modelling Mode") |> 
+                    class"f3 lh-title"
 
-    # Select between raster and network
-    data_type = vbox(Node(:div, "Select Data Type: "),
-                 Node(:select, "Select Data Type", 
-                 Node(:option, "Raster"), 
-                 Node(:option, "Network"), id = "dt", attributes = Dict(:style => "margin-top: 12px; margin-bottom: 12px")))
+    dt = get_data_type()
 
-    s = get_data_type!(data_type)
-    ui1 = network_ui()
-    ui2 = raster_ui()
-    nextui = map(s["value"]) do v
+    mod_mode_network = get_mod_mode_network()
+    mod_mode_raster = get_mod_mode_raster()
+
+    # Next drop down
+    nextui = map(dt["value"]) do v
         if v == "Network"
-            ui1
+            mod_mode_network
         else
-            ui2
+            mod_mode_raster
         end
     end
-
-    # boxes = [Node(:input, "Enter file path", attributes = Dict(:style => "margin: 12px")) for i = 1:10] 
+    
+    
 
     page = vbox(heading, 
-                hline(style = :solid)(style = Dict(:margin => 20px)), 
-                s, nextui) |> class"pa3 system-sans-serif"
-    
+                hline(style = :solid, w=5px)(style = Dict(:margin => 20px)), 
+                section1,
+                dt, nextui) |> class"pa3 system-sans-serif"
 
     body!(w, page)
 
 end
 
-function get_data_type!(data_type)
+function get_data_type()
+
+    data_type_prompt = "Step 1: Choose your input data type: "
+
+    # Select between raster and network
+    data_type = vbox(Node(:div, data_type_prompt, 
+                          attributes = Dict(:style => "margin-top: 12px")) |> class"b",
+                 Node(:select, "Select Data Type", 
+                 Node(:option, "Raster"), 
+                 Node(:option, "Network"), id = "dt", 
+                 attributes = Dict(:style => "margin-top: 12px; margin-bottom: 12px")))
 
     s = Scope()
     s.dom = data_type
@@ -55,10 +63,50 @@ function get_data_type!(data_type)
                     end)
              end)
 
-    on(s["value"]) do x
-        return s, x
-    end
     s
 end
 
+function get_mod_mode_network()
+    
+    mod_mode_prompt = "Step 2: Choose a Modelling Mode: "
+    mod_mode = vbox(Node(:div, mod_mode_prompt, 
+                          attributes = Dict(:style => "margin-top: 12px")) |> class"b",
+                 Node(:select, "Select Modelling Mode", 
+                 Node(:option, "Pairwise"), 
+                 Node(:option, "Advanced"), id = "mod", 
+                 attributes = Dict(:style => "margin-top: 12px; margin-bottom: 12px")))
+
+    s = Scope()
+    s.dom = mod_mode
+    onimport(s, JSExpr.@js function ()
+                 @var el = this.dom.querySelector("#mod")
+                 el.onchange = (function ()
+                        $(s["value"])[] = el.value
+                    end)
+             end)
+    s
+end
+
+function get_mod_mode_raster()
+    
+    mod_mode_prompt = "Step 2: Choose a Modelling Mode: "
+    mod_mode = vbox(Node(:div, mod_mode_prompt, 
+                          attributes = Dict(:style => "margin-top: 12px")) |> class"b",
+                 Node(:select, "Select Modelling Mode", 
+                 Node(:option, "Pairwise"), 
+                 Node(:option, "Advanced"), 
+                 Node(:option, "One To All "), 
+                 Node(:option, "All To One"), id = "mod", 
+                 attributes = Dict(:style => "margin-top: 12px; margin-bottom: 12px")))
+
+    s = Scope()
+    s.dom = mod_mode
+    onimport(s, JSExpr.@js function ()
+                 @var el = this.dom.querySelector("#mod")
+                 el.onchange = (function ()
+                        $(s["value"])[] = el.value
+                    end)
+             end)
+    s
+end
 generate_ui(w)
