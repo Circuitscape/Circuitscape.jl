504b8790e56a87cd869696e689545459e4eeb49d
ranjanan <benditlikeranjan@gmail.com>
2018-06-19 14:22:53 +0530

    Do not include missing file

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,6 @@ using Base.Test
 import Circuitscape: compute_single, compute_cholmod, compute_parallel,
                      runtests
 
-# Utility to compare output files
-include("compare_output.jl")
-
 # Unit tests for internals
 @testset "Unit tests" begin 
     include("internal.jl")
