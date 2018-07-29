93e39d33fdd02089c53db05d1bf7da6acb1aaed8
ranjanan <benditlikeranjan@gmail.com>
2018-07-28 21:19:11 -0400

    dummy 3

--- src/out.jl
+++ src/out.jl
@@ -173,7 +173,14 @@ function _get_node_currents_posneg(G::SparseMatrixCSC{T,V},
         branch_currents = branch_currents + spdiagm(finiteground_currents, 0, n, n)
     end
 
-    s = vec(sum(branch_currents, 1))
+    # s = vec(sum(branch_currents, 1))
+    s = Vector{T}(size(branch_currents, 2))
+    for i = 1:size(branch_currents, 1)
+        for j in nzrange(branch_currents, i)
+            s[i] += branch_currents.nzval[j]
+        end
+    end
+
     s
 end
 
