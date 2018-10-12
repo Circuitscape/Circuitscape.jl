a10037ab2511737932ddf47171ea8eff1a55e5ff
ranjanan <benditlikeranjan@gmail.com>
2018-10-12 18:02:14 -0400

    Improve error reporting on point map

--- src/io.jl
+++ src/io.jl
@@ -205,6 +205,17 @@ function read_point_map(V, file, habitatmeta)
     j = j[idx]
     v = v[idx]
 
+     if (minimum(j) < 0) || (minimum(v) < 0) ||
+             (maximum(j) > (habitatmeta.nrows - 1)) ||
+             (maximum(v) > (habitatmeta.ncols - 1))
+         throw("At least one focal node location falls outside of habitat map")
+     end
+
+     if size(unique(i),1) < 2
+         throw("Less than two valid focal nodes found. Please check focal node location file.")
+     end
+
+
     V.(i), V.(j), V.(v)
 end
 
