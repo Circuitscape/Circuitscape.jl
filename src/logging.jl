7cf5e893eeb78a405b7f22cc8b1a85feaf13928e
ranjanan <benditlikeranjan@gmail.com>
2018-11-21 18:22:26 -0500

    Format the dates

--- src/logging.jl
+++ src/logging.jl
@@ -1,8 +1,9 @@
-const fmt = "[{date} | {level} | {name}]: {msg}"
+# const fmt = "[{date} | {level} | {name}]: {msg}"
+const fmt = x -> Dates.format(x, "yyyy-mm-dd HH:MM:SS") 
 const ui_interface = Ref{Function}((x,y) -> nothing)
 
-csinfo(msg) = (@info(string(Dates.now()) * " : " * msg); ui_interface[](msg, :info))
-cswarn(msg) = (@warn(string(Dates.now()) * " : " * msg); ui_interace[](msg, :warn))
+csinfo(msg) = (@info(string(fmt(Dates.now())) * " : " * msg); ui_interface[](msg, :info))
+cswarn(msg) = (@warn(string(fmt(Dates.now())) * " : " * msg); ui_interace[](msg, :warn))
 
 function update_logging!(cfg)
 
