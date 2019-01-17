89fc634783e65e15c40212d3b638dfd3b3eaf913
ranjanan <benditlikeranjan@gmail.com>
2019-01-17 17:12:01 -0500

    Implement logging

--- src/logging.jl
+++ src/logging.jl
@@ -1,9 +1,37 @@
 # const fmt = "[{date} | {level} | {name}]: {msg}"
 const fmt = x -> Dates.format(x, "yyyy-mm-dd HH:MM:SS") 
 const ui_interface = Ref{Function}((x,y) -> nothing)
+const logging = Dict{String,Any}()
+logging["log_to_file"] = false
+logging["file_logger"] = SimpleLogger()
 
-csinfo(msg) = (@info(string(fmt(Dates.now())) * " : " * msg); ui_interface[](msg, :info))
-cswarn(msg) = (@warn(string(fmt(Dates.now())) * " : " * msg); ui_interface[](msg, :warn))
+function csinfo(msg) 
+    log_to_file = logging["log_to_file"]
+    file_logger = logging["file_logger"]
+    msg = string(fmt(Dates.now())) * " : " * msg 
+    @info(msg)
+    ui_interface[](msg, :info)
+    @show log_to_file
+    if log_to_file
+        with_logger(file_logger) do 
+            @info(msg)
+        end
+    end
+end
+
+function cswarn(msg)
+    log_to_file = logging["log_to_file"]
+    file_logger = logging["file_logger"]
+    msg = string(fmt(Dates.now())) * " : " * msg 
+    @warn(msg)
+    ui_interface[](msg, :warn)
+    @show log_to_file
+    if log_to_file
+        with_logger(file_logger) do 
+            @warn(msg)
+        end
+    end
+end
 
 function update_logging!(cfg)
 
@@ -14,5 +42,13 @@ function update_logging!(cfg)
         Logging.LogLevel(Logging.Debug)
     elseif log_level in WARNING
         Logging.LogLevel(Logging.Warn)
+    end
+
+    if log_file != "None"
+        logging["file_logger"] = SimpleLogger(open(log_file, "w+"))
+        logging["log_to_file"] = true
+    else
+        logging["file_logger"] = SimpleLogger()
+        logging["log_to_file"] = false
     end
 end
