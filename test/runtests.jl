20ea5d15d7205bc5ba7ed7565bedc1d4ab445556
Vincent Landau <vincent@csp-inc.org>
2020-05-16 23:17:19 -0600

    temporarily disable tests and sub in failing Mac code

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,22 @@ using Test
 import Circuitscape: compute_single, compute_cholmod, compute_parallel,
                      runtests
 
-# Unit tests for internals
-@testset "Unit tests" begin
-    include("internal.jl")
+filename = "test/input/raster/all_to_one/12/include_matrix.txt"
+
+f = endswith(filename, "gz") ? GZip.open(filename, "r") : open(filename, "r")
+filetype = Circuitscape._guess_file_type(filename, f)
+close(f)
+
+open(filename, "r") do f
+    minval = parse(Float64, split(readline(f))[2])
+    maxval = parse(Float64, split(readline(f))[2])
 end
 
-for f in (compute, compute_single, compute_cholmod, compute_parallel)
-    runtests(f)
-end
+## Unit tests for internals
+#@testset "Unit tests" begin
+#    include("internal.jl")
+#end
+
+#for f in (compute, compute_single, compute_cholmod, compute_parallel)
+#    runtests(f)
+#end
