7d3cfc6f2e10e106c21514d51eb4327f770902c8
Vincent Landau <vincent@csp-inc.org>
2020-05-18 12:11:37 -0600

    turned tests back on

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,22 +3,11 @@ using Test
 import Circuitscape: compute_single, compute_cholmod, compute_parallel,
                      runtests
 
-filename = "input/raster/all_to_one/12/include_matrix.txt"
-
-f = endswith(filename, "gz") ? GZip.open(filename, "r") : open(filename, "r")
-filetype = Circuitscape._guess_file_type(filename, f)
-close(f)
-
-open(filename, "r") do f
-    minval = parse(Float64, split(readline(f))[2])
-    maxval = parse(Float64, split(readline(f))[2])
+# Unit tests for internals
+@testset "Unit tests" begin
+    include("internal.jl")
 end
 
-## Unit tests for internals
-#@testset "Unit tests" begin
-#    include("internal.jl")
-#end
-
-#for f in (compute, compute_single, compute_cholmod, compute_parallel)
-#    runtests(f)
-#end
+for f in (compute, compute_single, compute_cholmod, compute_parallel)
+    runtests(f)
+end
