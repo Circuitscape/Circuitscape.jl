7db5f7a49e0a89af047e4fb8a96a6637154d7392
Vincent Landau <vincent.landau@gmail.com>
2024-02-09 17:45:11 -0700

    increase error tol to 1e-4 in some places I missed

--- src/raster/advanced.jl
+++ src/raster/advanced.jl
@@ -310,7 +310,7 @@ function multiple_solve(s::AMGSolver, matrix::SparseMatrixCSC{T,V}, sources::Vec
     csinfo("Time taken to construct preconditioner = $t1 seconds", suppress_info)
     t1 = @elapsed volt = solve_linear_system(matrix, sources, M)
     # @assert norm(matrix*volt .- sources) < (eltype(sources) == Float64 ? TOL_DOUBLE : TOL_SINGLE)
-	@assert (norm(matrix*volt .- sources) / norm(sources)) < 1e-6
+	@assert (norm(matrix*volt .- sources) / norm(sources)) < 1e-4
     csinfo("Time taken to solve linear system = $t1 seconds", suppress_info)
     volt
 end
@@ -319,7 +319,7 @@ function multiple_solve(s::CholmodSolver, matrix::SparseMatrixCSC{T,V}, sources:
     factor = construct_cholesky_factor(matrix, s, suppress_info)
     t1 = @elapsed volt = solve_linear_system(factor, matrix, sources)
     # @assert norm(matrix*volt .- sources) < (eltype(sources) == Float64 ? TOL_DOUBLE : TOL_SINGLE)
-	@assert (norm(matrix*volt .- sources) / norm(sources)) < 1e-6
+	@assert (norm(matrix*volt .- sources) / norm(sources)) < 1e-4
     csinfo("Time taken to solve linear system = $t1 seconds", suppress_info)
     volt
 end
@@ -328,7 +328,7 @@ function multiple_solve(s::MKLPardisoSolver, matrix::SparseMatrixCSC{T,V}, sourc
     factor = construct_cholesky_factor(matrix, s, suppress_info)
     t1 = @elapsed volt = solve_linear_system(factor, matrix, sources)
     # @assert norm(matrix*volt .- sources) < (eltype(sources) == Float64 ? TOL_DOUBLE : TOL_SINGLE)
-	@assert (norm(matrix*volt .- sources) / norm(sources)) < 1e-6
+	@assert (norm(matrix*volt .- sources) / norm(sources)) < 1e-4
     csinfo("Time taken to solve linear system = $t1 seconds", suppress_info)
     volt
 end
