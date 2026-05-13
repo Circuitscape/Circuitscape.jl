475962abd30eb8a513d66bf683629ce5b3fa8214
Viral B. Shah <viral@mayin.org>
2026-05-13 09:56:51 -0400

    Use Pinv as coarse solver

--- src/core.jl
+++ src/core.jl
@@ -161,7 +161,7 @@ function solve(prob::GraphProblem{T,V}, ::AMGSolver, flags, cfg, log)::Matrix{T}
         matrix.nzval .+= eps(eltype(matrix)) * norm(matrix.nzval)
 
         # Construct preconditioner *once* for every CC
-        P = @timeit CSTIMER "construct preconditioner" aspreconditioner(smoothed_aggregation(matrix))
+        P = @timeit CSTIMER "construct preconditioner" aspreconditioner(smoothed_aggregation(matrix); coarse_solver = AMG.Pinv)
 
         # Get local nodemap for CC - useful for output writing
         local_nodemap = @timeit CSTIMER "construct local nodemap" construct_local_node_map(nodemap, comp, polymap)
@@ -173,7 +173,7 @@ function solve(prob::GraphProblem{T,V}, ::AMGSolver, flags, cfg, log)::Matrix{T}
             # Each task needs its own workspace (scratch vectors are mutable)
             ml = P.ml
             local_P = aspreconditioner(AlgebraicMultigrid.MultiLevel(
-                ml.levels, ml.final_A, ml.coarse_solver,
+                ml.levels, ml.final_A, ml.coarse_solver = Pinv,
                 ml.presmoother, ml.postsmoother, deepcopy(ml.workspace)))
 
             results = Vector{Tuple{V,V,T}}()
